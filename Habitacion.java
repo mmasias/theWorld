87eecfef3836473c00d6fa548934d195ed8d6826
OmarBarrios <momarbarrios863@gmail.com>
2023-03-08 23:57:48 +0100

    Segunda iteracion

--- Habitacion.java
+++ Habitacion.java
@@ -1,6 +1,6 @@
 public class Habitacion {
 
-    private Baldosa[][] baldosas;
+    public Baldosa[][] baldosas;
     public int largo, ancho;
 
     public Habitacion(int largo, int ancho) {
@@ -28,45 +28,15 @@ public class Habitacion {
         imprimeLinea();
     }
 
-    public void imprimir(Gato[] gatos) {
-        char vistaBaldosa;
-        borraPantalla();
-        imprimeLinea();
-        for (int i = 0; i < largo; i++) {
-            for (int j = 0; j < ancho; j++) {
-                if (hayUnGato(gatos, i, j)) {
-                    System.out.print(">G<");
-                } else {
-                    vistaBaldosa = " .oO#".charAt(baldosas[i][j].ver());
-                    System.out.print(" " + vistaBaldosa + " ");
-                }
-            }
-            System.out.println();
-        }
-        imprimeLinea();
-    }
-
-    private boolean hayUnGato(Gato[] gatos, int i, int j) {
-        for (Gato gato : gatos) {
-            if (gato.posicionX == j && gato.posicionY == i) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public void ensuciar(int posicionX, int posicionY) {
         baldosas[posicionY][posicionX].ensucia();
     }
 
-    private void imprimeLinea() {
-        for (int j = 0; j < ancho; j++) {
-            System.out.print("===");
-        }
-        System.out.println();
+    public void imprimeLinea() {
+        System.out.println("============================================================");
     }
 
-    private void borraPantalla() {
+    public void borraPantalla() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
--- Mundo.java
+++ Mundo.java
@@ -14,13 +14,40 @@ class Mundo {
         }
     }
 
+    public void imprimir(Gato[] gatos) {
+        char vistaBaldosa;
+        habitacion.borraPantalla();
+        habitacion.imprimeLinea();
+        for (int i = 0; i < habitacion.largo; i++) {
+            for (int j = 0; j < habitacion.ancho; j++) {
+                if (hayUnGato(gatos, i, j)) {
+                    System.out.print(">G<");
+                } else {
+                    vistaBaldosa = " .oO#".charAt(habitacion.baldosas[i][j].ver());
+                    System.out.print(" " + vistaBaldosa + " ");
+                }
+            }
+            System.out.println();
+        }
+        habitacion.imprimeLinea();
+    }
+
+    private  boolean hayUnGato(Gato[] gatos, int i, int j) {
+        for (Gato gato : gatos) {
+            if (gato.posicionX == j && gato.posicionY == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     void empezar() {
         do {
             for (Gato gato : gatos) {
                 gato.mover(habitacion);
                 gato.ensuciar(habitacion);
             }
-            habitacion.imprimir(gatos);
+            imprimir(gatos);
         } while (quiereSeguir());
     }
 
